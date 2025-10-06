6293d17fe5be302621577ce8dab223503589a759
Moritz Beck <bkmoritz@web.de>
2025-10-06 16:15:18 +0200

    added get route for specific recipe

--- backend/app/api/routes/recipes.py
+++ backend/app/api/routes/recipes.py
@@ -1,6 +1,6 @@
 from typing import Any
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from sqlmodel import func, select
 
 from app.core.db import SessionDep
@@ -16,3 +16,11 @@ def read_recipes(session: SessionDep, skip: int = 0, limit: int = 100) -> Any:
     statement = select(Recipe).offset(skip).limit(limit)
     recipes = session.exec(statement).all()
     return RecipeOverview(data=recipes, count=count)
+
+
+@router.get("/{id}", response_model=Recipe)
+def read_recipe(session: SessionDep, id: str) -> Any:
+    recipe = session.get(Recipe, id)
+    if recipe is None:
+        raise HTTPException(status_code=404, detail="Recipe not found")
+    return recipe
